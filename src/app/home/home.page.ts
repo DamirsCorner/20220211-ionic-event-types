c423c3a7524ad4c40796a758caf42e8fcbd1580f
Damir Arh <damir.arh@gmail.com>
2022-02-06 09:14:26 +0100

    Handlers for CustomEvent types

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InputCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   constructor() {}
 
+  asInputCustomEvent(event: Event) {
+    return event as InputCustomEvent;
+  }
+
+  onInputChangeAny(inputEvent: any) {
+    console.log(inputEvent.detail.value);
+  }
+
+  onInputChangeEvent(event: Event) {
+    const inputEvent = event as InputCustomEvent;
+    console.log(inputEvent.detail.value);
+  }
+
+  onInputChangeCustom(inputEvent: InputCustomEvent) {
+    console.log(inputEvent.detail.value);
+  }
 }
